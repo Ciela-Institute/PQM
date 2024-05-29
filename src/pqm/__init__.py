d031335fb78a2be51aa2dd9bb16ae71f071347bf
Connor Stone <connorstone628@gmail.com>
2024-05-29 18:23:40 -0400

    fix init

--- src/pqm/__init__.py
+++ src/pqm/__init__.py
@@ -1,10 +1,10 @@
-from .pqm import get_pqm_pvalue
+from .pqm import pqm_pvalue
 from .test_gaussian import test
 from ._version import __version__
 
 
 __all__ = (
-    "get_pqm_pvalue",
+    "pqm_pvalue",
     "test",
     "__version__",
 )

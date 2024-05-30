27707bd83edf33241c538b3e2aacdaffc891129f
Sammy Sharief <sharief2@illinois.edu>
2024-05-29 23:03:47 -0400

    Removed MNIST from commit

--- README.md
+++ README.md
@@ -19,17 +19,12 @@ import numpy as np
 x_sample = np.random.normal(size = (500, 10))
 y_sample = np.random.normal(size = (400, 10))
 
-# To get pvalues from PQMass (this is the default case so you do not need to explicity write return_stat = "pvalue")
-pvalues = pqm_pvalue(x_sample, y_sample, num_refs = 100, bootstrap = 50, return_stat = "pvalue")
+# To get pvalues from PQMass
+pvalues = pqm_pvalue(x_sample, y_sample, num_refs = 100, bootstrap = 50)
 print(np.mean(pvalues), np.std(pvalues))
 
 # To get chi2 from PQMass
-chi2_stat = pqm_pvalue(x_sample, y_sample, num_refs = 100, bootstrap = 50, return_stat = "chi2")
-print(np.mean(chi2_stat), np.std(chi2_stat))
-
-# Can get both chi2 and pvalue 
-chi2_stat, pvalues = pqm_pvalue(x_sample, y_sample, num_refs = 100, bootstrap = 50, return_stat = "both")
-print(np.mean(pvalues), np.std(pvalues))
+chi2_stat = pqm_chi2(x_sample, y_sample, num_refs = 100, bootstrap = 50)
 print(np.mean(chi2_stat), np.std(chi2_stat))
 ```
 
--- src/pqm/pqm.py
+++ src/pqm/pqm.py
@@ -4,15 +4,15 @@ import numpy as np
 from scipy.stats import chi2_contingency
 from scipy.spatial import KDTree
 
-__all__ = "pqm_pvalue"
+__all__ = "pqm_chi2, pqm_pvalue"
+
+
 
 def pqm_pvalue(
     x_samples: np.ndarray,
     y_samples: np.ndarray,
     num_refs: int = 100,
-    bootstrap: Optional[int] = None,
-    return_stat: str = "pvalue"
-
+    bootstrap: Optional[int] = None
 ):
     """
     Perform the PQM test of the null hypothesis that `x_samples` and `y_samples` are drawn form the same distribution.
@@ -20,15 +20,13 @@ def pqm_pvalue(
     Parameters
     ----------
     x_samples : np.ndarray
-        Samples from the first distribution, samples from the posterior. Must have shape (N, *D) N is the number of x samples, and D is the dimensionality of the samples.
+        Samples from the first distribution, test samples Must have shape (N, *D) N is the number of x samples, and D is the dimensionality of the samples.
     y_samples : np.ndarray
-        Samples from the second distribution, truth samples. Must have shape (M, *D) M is the number of y samples, and D is the dimensionality of the samples.
+        Samples from the second distribution, reference samples. Must have shape (M, *D) M is the number of y samples, and D is the dimensionality of the samples.
     num_refs : int
         Number of reference samples to use. Note that these will be drawn from y_samples, and then removed from the y_samples array.
     bootstrap : Optional[int]
         Number of bootstrap iterations to perform. No bootstrap if None (default).
-    return_stat: str
-        The statistic to return from chi2_contingency. Must be 'pvalue', 'chi2', or 'both'.
 
     Returns
     -------
@@ -64,13 +62,70 @@ def pqm_pvalue(
     C = (counts_x > 0) | (counts_y > 0)
     counts_x, counts_y = counts_x[C], counts_y[C]
 
-    chi2, pvalue, _, _ = chi2_contingency(np.array([counts_x, counts_y]))
+    _, pvalue, _, _ = chi2_contingency(np.array([counts_x, counts_y]))
 
-    if return_stat == "pvalue":
-        return pvalue
-    elif return_stat == "chi2":
-        return chi2
-    elif return_stat == "both":
-        return chi2, pvalue
-    else:
-        raise ValueError("Invalid value for return_stat. Must be 'pvalue', 'chi2', or 'both'.")
+    return pvalue
+
+     
+
+    
+def pqm_chi2(
+    x_samples: np.ndarray,
+    y_samples: np.ndarray,
+    num_refs: int = 100,
+    bootstrap: Optional[int] = None,
+):
+    """
+    Perform the PQM test of the null hypothesis that `x_samples` and `y_samples` are drawn form the same distribution.
+
+    Parameters
+    ----------
+    x_samples : np.ndarray
+        Samples from the first distribution, test samples Must have shape (N, *D) N is the number of x samples, and D is the dimensionality of the samples.
+    y_samples : np.ndarray
+        Samples from the second distribution, reference samples. Must have shape (M, *D) M is the number of y samples, and D is the dimensionality of the samples.
+    num_refs : int
+        Number of reference samples to use. Note that these will be drawn from y_samples, and then removed from the y_samples array.
+    bootstrap : Optional[int]
+        Number of bootstrap iterations to perform. No bootstrap if None (default).
+
+    Returns
+    -------
+    float
+        pvalue. Null hypothesis that both samples are drawn from the same distribution.
+    """
+    if bootstrap is not None:
+        return list(pqm_pvalue(x_samples, y_samples, num_refs=num_refs) for _ in range(bootstrap))
+    if len(y_samples) < num_refs:
+        raise ValueError(
+            "Number of reference samples must be less than the number of true samples."
+        )
+    elif len(y_samples) < 2 * num_refs:
+        print(
+            "Warning: Number of y_samples is small (less than twice the number of reference samples). Result may have high variance."
+        )
+    refs = np.random.choice(len(y_samples), num_refs, replace=False)
+    N = np.arange(len(y_samples))
+    N[refs] = -1
+    N = N[N >= 0]
+    refs, y_samples = y_samples[refs], y_samples[N]
+
+    counts_x = np.zeros(num_refs, dtype="int")
+    counts_y = np.zeros(num_refs, dtype="int")
+    for x in x_samples:
+        d = np.linalg.norm(x.reshape(1, -1) - refs, axis=-1)
+        idx = np.argmin(d)
+        counts_x[idx] += 1
+
+    for y in y_samples:
+        d = np.linalg.norm(y.reshape(1, -1) - refs, axis=-1)
+        idx = np.argmin(d)
+        counts_y[idx] += 1
+
+    # Remove reference samples with no counts
+    C = (counts_x > 0) | (counts_y > 0)
+    counts_x = counts_x[C]
+    counts_y = counts_y[C]
+
+    chi2_stat, _, _, _ = chi2_contingency(np.array([counts_x, counts_y]))
+    return chi2_stat

4aa8466f3ad169bcaca5637390d9c54755279e4e
Connor Stone <connorstone628@gmail.com>
2024-07-19 09:38:30 -0400

    Set default version for developers

--- src/pqm/__init__.py
+++ src/pqm/__init__.py
@@ -1,6 +1,11 @@
 from .pqm import pqm_pvalue, pqm_chi2
 from .test_gaussian import test
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0-dev"
+
 
 __all__ = (
     "pqm_pvalue",
